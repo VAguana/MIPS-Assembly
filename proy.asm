ad5a59e37b52691c59e8a1f49b1da8970fb5b957
LDiazN <15-10420@usb.ve>
2019-03-04 09:46:40 -0400

    añadida función init, variable local inithead que guarda el inicio del segmento de memoria dado

--- proy.asm
+++ proy.asm
@@ -1,7 +1,48 @@
 
-li $v0, 9
-addi $a0, $zero, 8 #cantidad de bytes
-syscall
+.data
 
-add $v1, $v0, $zero
+msg1:	.asciiz  "\n"
+initHead: .word 0
 
+
+.text
+main:
+	#print de prueba:
+	la $a0 initHead
+	li $v0  1
+	syscall
+
+	la $a0 msg1
+	li $v0 4
+	syscall
+
+	addi $a0 $zero 4
+	jal init
+	
+	#print de prueba:
+	la $a0 initHead
+	li $v0  4
+	syscall
+	
+	jal end
+
+
+
+
+init:
+	#Se llama a init con los argumentos que hay en $a0
+	li $v0, 9
+	syscall
+	
+	sw $v0 initHead
+	jr $ra
+
+test:
+	la $a0 msg1
+	addi $v0 $zero 4
+	syscall
+	jr $ra
+	
+end:
+	li $v0 10
+	syscall
